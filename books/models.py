1106772c457c3fa9067ef9ae46cc49f06a9c703b
Serhii <sergei.amvarcumov@gmail.com>
2026-09-14 19:16:23 +0200

    implement Book model, serializer, and viewset

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", "Hardcover"
+        SOFT = "SOFT", "Paperback"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices.choices,
+        default=CoverChoices.HARD,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.author}"
